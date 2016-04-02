69a19ceb9802204942b7edf346e80f6ea6f600de
Nic Jackson <jackson.nic@gmail.com>
2016-04-02 18:29:25 +0100

    added headers

--- shim.h
+++ shim.h
@@ -1,1 +1,11 @@
+#ifndef LibMySQL_h
+#define LibMySQL_h
 
+#include <my_global.h>
+#include <mysql.h>
+
+extern inline get_client_info() {
+    printf("MySQL client version: %s\n", mysql_get_client_info());
+}
+
+#endif
