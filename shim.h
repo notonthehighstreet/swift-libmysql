6f8b63366b2c637aeec3483afa7861c63c4fd7e0
Nic Jackson <jackson.nic@gmail.com>
2016-04-03 09:56:17 +0100

    removed helper functions as they are not required

--- shim.h
+++ shim.h
@@ -4,12 +4,4 @@
 #include "include/my_global.h"
 #include "include/mysql.h"
 
-extern inline const char* get_client_info() {
-    return mysql_get_client_info();
-}
-
-extern inline unsigned long	get_client_version() {
-  return mysql_get_client_version();
-}
-
 #endif
