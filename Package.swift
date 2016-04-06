14306c718ab252d3e2a0c32011a7a40d2bda16e8
Nic Jackson <nicj@kewm1496.local>
2016-04-06 21:27:48 +0100

    updated to remove bundled binaries, added includes for darwin

--- Package.swift
+++ Package.swift
@@ -1,13 +1,1 @@
-#if os(Linux)
-import Glibc
-#else
-import Darwin
-#endif
 
-system("mkdir -p .build/debug")
-
-#if os(Linux)
-    system("cp `find Packages/ -type d -name swift-libmysql*`/lib/linux-64/* .build/debug")
-#else
-    system("cp `find Packages/ -type d -name darwin*`/lib/linux-64/* .build/debug")
-#endif
--- shim.h
+++ shim.h
@@ -1,7 +1,12 @@
 #ifndef LibMySQL_h
 #define LibMySQL_h
 
-#include "include/my_global.h"
-#include "include/mysql.h"
+#ifdef __unix__
+#include "include-linux/my_global.h"
+#include "include-linux/mysql.h"
+#else
+#include "include-darwin/my_global.h"
+#include "include-darwin/mysql.h"
+#endif
 
 #endif
